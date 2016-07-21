82d5bcf1de3fc10ee5000d5c599426c7765b2e7c
Natalia Glazkina <glazatkina@gmail.com>
2016-07-21 10:50:31 +0300

    Simulation of keys pressing

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <QtWidgets>
+
+int main(int argc, char* argv[])
+{
+	QApplication app(argc, argv);
+
+	QLineEdit txt("User input: ");
+	txt.show();
+	txt.resize(300, 20);
+
+	int i;
+	for (i = 0; i < Qt::Key_Z - Qt::Key_A + 1; ++i) {
+		QChar ch = 65 + i;
+		int nKey = Qt::Key_A + i;
+		QKeyEvent keyPress(QEvent::KeyPress, nKey, Qt::NoModifier, ch);
+		QApplication::sendEvent(&txt, &keyPress);
+
+		QKeyEvent keyRelease(QEvent::KeyRelease, nKey, Qt::NoModifier, ch);
+		QApplication::sendEvent(&txt, &keyRelease);
+	}
+	return app.exec();
+};
